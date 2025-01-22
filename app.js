5cf968a539bd661d744a166e6e344e8451724e9c
Danylo Baziewicz <danxbazi@gmail.com>
2025-01-22 15:54:02 -0300

    criadas funções base para incio do projeto.

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomeAmigo = '';
+let ListaNomeAmigo = [];
+
+function recebeNome(){
+    nomeAmigo = document.querySelector('input').value;
+    // console.log(nomeAmigo);
+}
+
+function adicionarAmigo() {
+    recebeNome();
+    if (nomeAmigo == '') {
+        alert('Ops! Digite o nome de um amigo!')
+    } else {
+        if (ListaNomeAmigo.includes(nomeAmigo)){
+            alert(`${nomeAmigo} já foi incluído, tente adicionar algo mais para melhorar a identificação`)
+        } else {
+            ListaNomeAmigo.push(nomeAmigo);
+            // console.log(ListaNomeAmigo)
+            limparCampo();
+            exibirListaAmigos();
+        }
+    }
+}
+
+function exibirListaAmigos(){
+    nomeAmigo = document.getElementById('listaAmigos');
+    nomeAmigo.innerHTML = ListaNomeAmigo;
+    console.log(`func exibirListaAmigos ${ListaNomeAmigo}`);
+}
+
+function limparCampo(){
+    nomeAmigo = document.querySelector('input');
+    nomeAmigo.value = '';
+}
