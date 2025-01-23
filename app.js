721f5cfc7ad144d58b52924e3ac115ab35ea9206
Danylo Baziewicz <danxbazi@gmail.com>
2025-01-23 20:47:53 -0300

    comentários removidos para finalização do projeto.

--- app.js
+++ app.js
@@ -4,19 +4,17 @@ let listItem;
 
 function recebeNome(){
     nomeAmigo = document.querySelector('input').value;
-    // console.log(nomeAmigo);
 }
 
 function adicionarAmigo() {
     recebeNome();
     if (nomeAmigo == '') {
-        alert('Ops! Digite o nome de um amigo!')
+        alert('Por favor, insira um nome')
     } else {
         if (listaNomeAmigo.includes(nomeAmigo)){
             alert(`${nomeAmigo} já foi incluído, tente adicionar algo mais para melhorar a identificação`)
         } else {
             listaNomeAmigo.push(nomeAmigo);
-            // console.log(listaNomeAmigo)
             limparCampo();
             exibirListaAmigos();
         }
@@ -26,31 +24,23 @@ function adicionarAmigo() {
 
 function exibirListaAmigos(){
     let listaUl = document.getElementById('listaAmigos');
-    let novoAmigo = listaNomeAmigo[listaNomeAmigo.length - 1]; // Pega o último nome adicionado
+    let novoAmigo = listaNomeAmigo[listaNomeAmigo.length - 1]; 
     let listItem = document.createElement('li');
     listItem.textContent = novoAmigo;
     listaUl.appendChild(listItem);
 }
 
 function sortearAmigo() {
-    if (listaNomeAmigo.length === 0 || listaNomeAmigo.length === 1) { // Verifica se a lista está vazia
+    if (listaNomeAmigo.length === 0 || listaNomeAmigo.length === 1) { 
         alert(`Ops! Antes de sortear, adicione dois ou mais nomes dos seus amigos!`);
     } else {
-        let indiceAleatorio = Math.floor(Math.random() * listaNomeAmigo.length); // Gera um índice aleatório
-        let amigoSorteado = listaNomeAmigo[indiceAleatorio]; // Pega o nome do amigo no índice sorteado
-        
-        // Seleciona o elemento <ul> onde os amigos estão listados
+        let indiceAleatorio = Math.floor(Math.random() * listaNomeAmigo.length); 
+        let amigoSorteado = listaNomeAmigo[indiceAleatorio]; 
         let listaUl = document.getElementById('listaAmigos');
-        
-        // Limpa a lista visualmente antes de adicionar o sorteio
         listaUl.innerHTML = '';
-
-        // Cria um novo <li> com a mensagem do sorteio
         listItem = document.createElement('li');
-        listItem.textContent = `O amigo sorteado é: ${amigoSorteado}`; // Exibe o nome sorteado
-        listaUl.appendChild(listItem); // Adiciona o <li> à lista
-        
-        // Limpa a lista de amigos no array
+        listItem.textContent = `O amigo sorteado é: ${amigoSorteado}`;
+        listaUl.appendChild(listItem);
         listaNomeAmigo = [];
     }
 }
@@ -65,7 +55,7 @@ function limparCampo(){
 }
 
 document.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {  // Verifica se a tecla pressionada foi "Enter"
-        adicionarAmigo(); // Chama a função do sorteio
+    if (event.key === 'Enter') {
+        adicionarAmigo();
     }
 });
